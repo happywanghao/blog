89986d732d43e957e0e205fe8bd1e513843710e8
happywanghao <654450587@qq.com>
2017-06-30 16:31:36 +0800

    更改card

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -4,12 +4,16 @@ import {Link,withRouter} from 'react-router-dom';
 class BlogCard extends React.Component {
   render () {
     let styles={
+      root:{
+        margin:'0 10px 0 10px',
+      },
       index:{
+        float:'left',
         backgroundColor:'#00bcd4',
-        height:'50px',
+        height:'90px',
+        width:'10%',
         textAlign:'center',
-        borderRadius: '10px 10px 0 0',
-        paddingTop:'10px'
+        paddingTop:'27px'
       },
       num:{
         lineHeight:'35px',
@@ -25,17 +29,16 @@ class BlogCard extends React.Component {
     }
     let address = `/detailed/blog/${this.props.url}`
     return(
-      <div className="col-sm-6 col-md-4">
-        <div className="thumbnail">
+      <div style={styles.root}>
+        <div style={{position:'relative'}} className="thumbnail clearfix">
           {/* <img alt='头像' src={this.props.img} /> */}
-          <div style={styles.index}><span style={styles.num}>{this.props.index+1}</span></div>
-          <div className="caption">
-            <h3 dangerouslySetInnerHTML={{__html:this.props.title}}/>
-            <p>
-              <Link to={address} className="btn btn-primary" role="button" style={{marginRight:'10px'}}>查看详情</Link>
-            </p>
-          </div>
+            <div style={styles.index}><span style={styles.num}>{this.props.index+1}</span></div>
+            <div style={{float:'left',width:'90%',}}>
+              <h3 dangerouslySetInnerHTML={{__html:this.props.title}}/>
+              <Link to={address} className="btn btn-primary" role="button" style={{position:'absolute',bottom:'5px',right:'5px'}}>查看详情</Link>
+            </div>
         </div>
+
       </div>
     )
   }
